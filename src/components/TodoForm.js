e9ff89cee8159edf74c1b8d1e1485482d70dda9c
zrtmsn <artemison04@gmail.com>
2023-07-21 04:26:44 +0200

    add something

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export const TodoForm = () => {
     return (
-        <div>TodoForm</div>
+        <form className="TodoForm">
+            <input type="text" className="todo-input" placeholder=" what is a task today?"/>
+            <button type="submit" className="todo-btn"></button>
+        </form>
     )
 }
